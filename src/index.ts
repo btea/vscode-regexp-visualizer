cf67443694239eba8f1f4d79f3ec7f439f29884c
btea <2356281422@qq.com>
2025-03-23 11:56:34 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,51 @@
 import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import * as vscode from 'vscode'
 
-const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+const { activate, deactivate } = defineExtension((context) => {
+  const disposable = vscode.commands.registerCommand('regexp-visualizer.show', () => {
+    const editor = vscode.window.activeTextEditor
+    if (!editor) {
+      return
+    }
+    const selection = editor.selection
+    let text = editor.document.getText(selection)
+    if (!text) {
+      return
+    }
+    const validRegex = /^\/.*\/[gimuy]*$/
+    if (!validRegex.test(text)) {
+      vscode.window.showErrorMessage('Invalid regex')
+      return
+    }
+    const panel = vscode.window.createWebviewPanel(
+      'regexVisualizer',
+      'Regex Visualizer',
+      vscode.ViewColumn.Two,
+      {
+        enableScripts: true,
+        retainContextWhenHidden: true,
+      }
+    )
+
+    const flags = text.match(/\/([gimuy]*)$/)?.[1] || ''
+    text = text.replace(/^\/|\/[gimuy]*$/g, '').replace(/^\/|\/$/, '')
+    text = encodeURIComponent(text)
+    const url = `https://jex.im/regulex/#!flags=${flags}&re=${text}`
+    const str  = `
+<html>
+  <head>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src vscode-resource: 'unsafe-inline'; style-src vscode-resource: 'unsafe-inline'; frame-src https:;">
+  </head>
+  <body style="height: 100vh; margin: 0; padding: 0;">
+    <iframe src="${url}" style="width: 100%; height: 100%; border: none;"></iframe>
+  </body>
+</html>
+`;
+    panel.webview.html = str
+  })
+
+  context.subscriptions.push(disposable)
+  
 })
 
 export { activate, deactivate }
