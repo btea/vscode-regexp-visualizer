fd34de7e22ba70f9d9817a1d56737c99266cc9aa
btea <2356281422@qq.com>
2025-03-23 11:58:10 +0800

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,14 @@ const { activate, deactivate } = defineExtension((context) => {
       {
         enableScripts: true,
         retainContextWhenHidden: true,
-      }
+      },
     )
 
     const flags = text.match(/\/([gimuy]*)$/)?.[1] || ''
     text = text.replace(/^\/|\/[gimuy]*$/g, '').replace(/^\/|\/$/, '')
     text = encodeURIComponent(text)
     const url = `https://jex.im/regulex/#!flags=${flags}&re=${text}`
-    const str  = `
+    const str = `
 <html>
   <head>
     <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src vscode-resource: 'unsafe-inline'; style-src vscode-resource: 'unsafe-inline'; frame-src https:;">
@@ -40,12 +40,11 @@ const { activate, deactivate } = defineExtension((context) => {
     <iframe src="${url}" style="width: 100%; height: 100%; border: none;"></iframe>
   </body>
 </html>
-`;
+`
     panel.webview.html = str
   })
 
   context.subscriptions.push(disposable)
-  
 })
 
 export { activate, deactivate }
